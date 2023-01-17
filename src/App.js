4e157912f41ab9476545f64dcd56abac0926da2d
Siddharth Kalyani <siddharthkalyani@Siddharths-MacBook-Pro.local>
2023-01-17 13:49:41 +0530

    Set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        
+    
       </header>
-    </div>
+      <main>
+
+      </main>  
+    </>
   );
 }
 
